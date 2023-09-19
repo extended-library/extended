e7a8abb9ea60cde894d68d8915341f81aceaa134
Richard King <richrdkng@gmail.com>
2023-09-19 13:26:54 +0200

    chore(root): temporarily disable 100% coverage requirements

--- shared/jest.config.js
+++ shared/jest.config.js
@@ -2,14 +2,14 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['jest-extended/all'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 100,
+  //     functions: 100,
+  //     lines: 100,
+  //     statements: 100
+  //   }
+  // },
   coverageReporters: [
     'lcov',
     'text'
