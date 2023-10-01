623a13e0fad863c3c94350f0365135a094dc9f7f
Richard King <richrdkng@gmail.com>
2023-10-01 20:52:01 +0200

    test(types): add getResultWithSuccess and getResultWithError tests

--- packages/types/src/Result/index.test.ts
+++ packages/types/src/Result/index.test.ts
@@ -1,48 +1,74 @@
 import { createResult } from '.'
 
 describe('@extended/types/Result', () => {
-  test('basic usage - success', () => {
-    const { setSuccess, getResult } = createResult<number>()
+  describe('basic usage', () => {
+    test('basic usage - success', () => {
+      const { setSuccess, getResult } = createResult<number>()
 
-    setSuccess(1)
+      setSuccess(1)
 
-    const result = getResult()
+      const result = getResult()
 
-    expect(result.success).toBeTrue()
-    expect(result.error).toBeUndefined()
-    expect(result.result).toBe(1)
+      expect(result.success).toBeTrue()
+      expect(result.error).toBeUndefined()
+      expect(result.result).toBe(1)
+    })
+
+    test('basic usage - error', () => {
+      const { setError, getResult } = createResult<number>()
+
+      setError(new TypeError())
+
+      const result = getResult()
+
+      expect(result.success).toBeFalse()
+      expect(result.error).toBeInstanceOf(TypeError)
+      expect(result.result).toBeUndefined()
+    })
+
+    test('basic usage - result destructuring', () => {
+      const { setSuccess, getResult } = createResult<string>()
+
+      setSuccess('abc')
+
+      const { success, error, result } = getResult()
+
+      expect(success).toBeTrue()
+      expect(error).toBeUndefined()
+      expect(result).toBe('abc')
+    })
   })
 
-  test('basic usage - error', () => {
-    const { setError, getResult } = createResult<number>()
+  describe('shorthand', () => {
+    test('basic shorthand - success', () => {
+      const { getResultWithSuccess } = createResult<number>()
 
-    setError(new TypeError())
+      const result = getResultWithSuccess(1)
 
-    const result = getResult()
+      expect(result.success).toBeTrue()
+      expect(result.error).toBeUndefined()
+      expect(result.result).toBe(1)
+    })
 
-    expect(result.success).toBeFalse()
-    expect(result.error).toBeInstanceOf(TypeError)
-    expect(result.result).toBeUndefined()
+    test('basic shorthand - error', () => {
+      const { getResultWithError } = createResult<number>()
+
+      const result = getResultWithError(new TypeError())
+
+      expect(result.success).toBeFalse()
+      expect(result.error).toBeInstanceOf(TypeError)
+      expect(result.result).toBeUndefined()
+    })
   })
 
-  test('basic usage - result destructuring', () => {
-    const { setSuccess, getResult } = createResult<string>()
+  describe('errors', () => {
+    test('result never set error', () => {
+      const { getResult } = createResult()
 
-    setSuccess('abc')
-
-    const { success, error, result } = getResult()
-
-    expect(success).toBeTrue()
-    expect(error).toBeUndefined()
-    expect(result).toBe('abc')
-  })
-
-  test('result never error', () => {
-    const { getResult } = createResult()
-
-    expect(() => getResult()).toThrowWithMessage(
-      Error,
-      'Result was never set, call setSuccess() or setError() first.'
-    )
+      expect(() => getResult()).toThrowWithMessage(
+        Error,
+        'Result was never set, call setSuccess() or setError() first.'
+      )
+    })
   })
 })
