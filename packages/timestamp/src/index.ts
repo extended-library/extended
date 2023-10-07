f938e3821a5ded71e0dc7e7e20775f659122a358
Richard King <richrdkng@gmail.com>
2023-10-07 23:31:02 +0200

    feat(timestamp): add UTCTimestamp and createTimestamp utility functions

--- packages/timestamp/src/index.ts
+++ packages/timestamp/src/index.ts
@@ -1,80 +1,105 @@
+import DateAdapter from './utils/DateAdapter'
+
 import {
-  getYear,
-  getMonth,
-  getDay,
-  getHour,
-  getMinutes,
-  getSeconds,
-  getMilliseconds,
-  getOffset
-} from './utils'
+  type Preset,
+  isPreset,
+  getPreset
+} from './utils/preset'
+
+import format from './utils/format'
 
 // LOG -> ISO 8601 -> '2017-09-14T03:20:34.091-04:00'
 
-type Preset = 'LOG' | 'LOG-SHORT' | 'FILE' | 'FILE-SHORT'
+// -----------------------------------------------------------------------------
 
-interface Options {
-  preset?: Preset
-  format?: string
-  from?: string
+interface Options<T extends string> {
+  format?: T
+  UTC?: boolean
+  from?: string | Date
 }
+
+// -----------------------------------------------------------------------------
 
 function timestamp (): string
-function timestamp (format: string): string
+
 function timestamp (preset: Preset): string
-function timestamp (options: Options): string
+
+function timestamp (format: string): string
+
+function timestamp (options: Options<Preset>): string
+
+function timestamp (options: Options<string>): string
 
 function timestamp (arg1?: any): string {
-  const date = new Date()
-  // // var offset = localDate.getTimezoneOffset();
-  // // var utc = new Date(localDate.getTime() - offset * 60000);
+  const dateAdapter = new DateAdapter(new Date(), false)
 
-  // // console.log(offset)
-  // // console.log(utc)
-
-  // console.log(new Date().toString())
-  // console.log(new Date().toISOString())
-
-  // console.log(new Date().getHours())
-  // console.log(new Date().getUTCHours())
-
-  if (typeof arg1 === 'undefined' || arg1 === 'LOG') {
-    return (
-      getYear(date) + '-' + getMonth(date) + '-' + getDay(date) +
-      'T' +
-      getHour(date) + ':' + getMinutes(date) + ':' + getSeconds(date) +
-      '.' + getMilliseconds(date) +
-      getOffset(date)
-    )
+  if (typeof arg1 === 'undefined') {
+    return getPreset(dateAdapter, 'LOG')
   }
 
-  if (arg1 === 'LOG-SHORT') {
-    return (
-      getYear(date) + '-' + getMonth(date) + '-' + getDay(date) +
-      'T' +
-      getHour(date) + ':' + getMinutes(date) + ':' + getSeconds(date)
-    )
+  if (typeof arg1 === 'string') {
+    return isPreset(arg1)
+      ? getPreset(dateAdapter, arg1 as Preset)
+      : format(arg1, dateAdapter)
+  } else if (typeof arg1 === 'object' && arg1 !== null) {
+    if (typeof arg1.UTC === 'boolean') {
+      dateAdapter.UTC = arg1.UTC
+    }
+
+    if (isPreset(arg1.preset)) {
+      return getPreset(dateAdapter, arg1 as Preset)
+    }
+
+    if (typeof arg1.format === 'string') {
+      return format(arg1.format, dateAdapter)
+    }
   }
 
-  if (arg1 === 'FILE') {
-    return (
-      getYear(date) + '-' + getMonth(date) + '-' + getDay(date) +
-      'T' +
-      getHour(date) + '-' + getMinutes(date) + '-' + getSeconds(date) +
-      '-' + getMilliseconds(date) +
-      getOffset(date, '-')
-    )
-  }
-
-  if (arg1 === 'FILE-SHORT') {
-    return (
-      getYear(date) + '-' + getMonth(date) + '-' + getDay(date) +
-      'T' +
-      getHour(date) + '-' + getMinutes(date) + '-' + getSeconds(date)
-    )
-  }
-
-  return timestamp()
+  return getPreset(dateAdapter, 'LOG')
 }
 
-export default timestamp
+// -----------------------------------------------------------------------------
+
+function UTCTimestamp (): string
+
+function UTCTimestamp (preset: Preset): string
+
+function UTCTimestamp (format: string): string
+
+function UTCTimestamp (options: Options<Preset>): string
+
+function UTCTimestamp (options: Options<string>): string
+
+function UTCTimestamp (arg1?: any): string {
+  return timestamp({ UTC: true })
+}
+
+// -----------------------------------------------------------------------------
+
+function createTimestamp (preset: Preset): typeof timestamp
+
+function createTimestamp (format: string): typeof timestamp
+
+function createTimestamp (options: Options<Preset>): typeof timestamp
+
+function createTimestamp (options: Options<string>): typeof timestamp
+
+function createTimestamp (arg1?: any): typeof timestamp {
+  return () => timestamp()
+}
+
+// -----------------------------------------------------------------------------
+
+const extendedTimestamp: typeof timestamp & {
+  createTimestamp: typeof createTimestamp
+  UTCTimestamp: typeof UTCTimestamp
+} = timestamp as any
+
+extendedTimestamp.createTimestamp = createTimestamp
+extendedTimestamp.UTCTimestamp = UTCTimestamp
+
+export default extendedTimestamp
+
+// ;(() => {
+//   timestamp('-YYYY-MM-')
+// })()
--- packages/timestamp/test/index.test.ts
+++ packages/timestamp/test/index.test.ts
@@ -1,4 +1,8 @@
+import { DateTime } from 'luxon'
+
 import timestamp from '../src'
+
+const localDate = DateTime.now()
 
 describe('@extended/timestamp', () => {
   test('default timestamp (YYYY-MM-DDTHH:mm:ss:ms+##:##)', () => {
@@ -36,6 +40,51 @@ describe('@extended/timestamp', () => {
       expect(timestamp('FILE-SHORT')).toMatch(
         /^\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}$/
       )
+    })
+  })
+
+  describe('formatting', () => {
+    describe('single patterns', () => {
+      test('year (YYYY)', () => {
+        expect(timestamp('YYYY')).toBe(localDate.toFormat('yyyy'))
+      })
+
+      test('month (MM)', () => {
+        expect(timestamp('MM')).toBe(localDate.toFormat('MM'))
+      })
+
+      test('day (DD)', () => {
+        expect(timestamp('DD')).toBe(localDate.toFormat('dd'))
+      })
+
+      test('hours (HH)', () => {
+        expect(timestamp('HH')).toBe(localDate.toFormat('HH'))
+      })
+
+      test('minutes (mm)', () => {
+        expect(timestamp('mm')).toBe(localDate.toFormat('mm'))
+      })
+
+      test('seconds (ss)', () => {
+        expect(timestamp('ss')).toBe(localDate.toFormat('ss'))
+      })
+
+      test('milliseconds (ms)', () => {
+        expect(timestamp('ms')).toMatch(/^\d{3}$/)
+      })
+    })
+
+    describe('multiple patterns', () => {
+      test('year-month-day (YYYY-MM-DD)', () => {
+        expect(timestamp('YYYY-MM-DD')).toBe(localDate.toFormat('yyyy-MM-dd'))
+      })
+    })
+  })
+
+  describe('UTC timestamp', () => {
+    test('x', () => {
+      console.log(timestamp.UTCTimestamp())
+      // expect(timestamp.UTCTimestamp())
     })
   })
 })
